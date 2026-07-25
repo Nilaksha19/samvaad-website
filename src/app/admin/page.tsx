a2621bed03fdcf4f3bd06b338fb99260f8b000fd
Nilaksha Sinha Roy <nilakshasinharoy2@gmail.com>
2026-07-25 23:19:57 +0530

    Fix components folder casing

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import AdminSignOutButton from "@/components/AdminSignOutButton";
+import AdminSignOutButton from "@/Components/AdminSignOutButton";
 
 export default function AdminDashboardPage() {
   return (
